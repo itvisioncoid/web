882d0711319e430adeff39cafe33d784099246d8
PDev <riel.cfb@gmail.com>
2023-02-21 04:11:33 +0800

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+// import { defineConfig } from 'vitest/config';
+import type { UserConfig } from 'vite';
 
-export default defineConfig({
+const config: UserConfig = {
+	logLevel: 'info',
+	
+	build: {
+		minify: true,
+	},
+	
 	plugins: [sveltekit()],
+	
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
-});
+};
+
+export default config;

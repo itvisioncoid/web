f8f77badb9ff258f6e3ed154be5b4abd3de427ea
PDev <riel.cfb@gmail.com>
2023-02-21 04:13:34 +0800

    use `app styles` and use `google fonts`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,13 @@
 <script>
-    import '@picocss/pico';
+    // import '@picocss/pico';
+    import '../app.scss';
+    // import '../custom.css';
 </script>
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+</svelte:head>
+
 <slot />

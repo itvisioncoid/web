a53ebc0931406abb775782de976dfb0a3c643d67
PDev <riel.cfb@gmail.com>
2023-02-21 04:11:15 +0800

    use preprocess config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+// import autoprefixer from 'autoprefixer';
+import * as path from 'path';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		adapter: adapter()

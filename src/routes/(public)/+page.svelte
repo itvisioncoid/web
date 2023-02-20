283c7730cb581867d87caa1d40aca85cb54144b8
PDev <riel.cfb@gmail.com>
2023-02-21 04:17:00 +0800

    create typing animated `motto` text on `home page`

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,13 +1,41 @@
-<div class="hero" data-theme="dark">
+<script lang="ts">
+import { onDestroy, onMount } from 'svelte';
+import Typed from 'typed.js';
+
+let typeitEl: HTMLElement;
+let typed: Typed;
+
+const startTypit = () => {
+	typed = new Typed(typeitEl, {
+		strings: ["Better Vision, Better Solution"],
+		typeSpeed: 70,
+		loop: false,
+		showCursor: false,
+		autoInsertCss: false
+	});
+	// typed.start();
+	return typed.destroy;
+}
+
+onMount(() => {
+	startTypit();
+});
+
+onDestroy(() => {
+	typed.destroy();
+})
+</script>
+
+<section class="hero" data-theme="dark">
 	<header class="container">
 		<hgroup>
 			<h1>CV. IT VISION</h1>
 			<h2>
-				<em>Better Vision, Better Solution</em>
+				<em bind:this={typeitEl}></em>
 			</h2>
 		</hgroup>
 		<p>
 			<a href="/" role="button" on:click={(event) => event.preventDefault()}>Contact Us</a>
 		</p>
 	</header>
-</div>
+</section>

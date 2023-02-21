bb4dc1358a93193eb2217735084a484869d6a8fe
haekal404 <hulaimihaikal@gamil.com>
2023-02-21 18:41:20 +0800

    update page-about

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
 </svelte:head>
 
 <slot />

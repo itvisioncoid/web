7553744fca4bb3d68d89f63cefbfd865c3e7b414
PDev <riel.cfb@gmail.com>
2023-02-21 18:45:27 +0800

    add `mobile navigation` menu

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,22 +1,56 @@
 <script lang="ts">
-	// import '@picocss/pico';
+	// import '$src/app.scss';
 
 	const date = new Date();
+	const menuLists = [
+		{ text: 'Product', url: '/product' },
+		{ text: 'Services', url: '/services' },
+		{ text: 'Contact Us', url: '/contact' },
+		{ text: 'About Us', url: '/about' },
+	];
+	let hideNav = true;
 </script>
+
 <svelte:head>
 	<title>Home - IT Vision</title>
 </svelte:head>
 
-<nav class="container">
-	<ul>
+<div id="mobile-nav" class="mobile-nav" aria-hidden="{hideNav}" aria-label="mobile-nav" on:click|self={() => (hideNav = true)}>
+	<aside>
+		<nav>
+			<ul>
+				{#each menuLists as menu}
+				<li>
+					<a href="{ menu.url }">{ menu.text }</a>
+				</li>
+				{/each}
+			</ul>
+		</nav>
+	</aside>
+</div>
+
+<nav class="container" aria-label="main-nav">
+	<ul class="brand">
 		<li>
 			<a href="/" class="contrast" aria-label="brand">
 				<strong>IT Vision</strong>
 			</a>
 	  	</li>
+		<li class="show-sm-only">
+			<a href="#mobile-nav" aria-label="Close menu" aria-controls="mobile-nav" on:click|preventDefault={() => (hideNav = !hideNav)}>
+			  <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="16px" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+				<line x1="3" y1="12" x2="21" y2="12"></line>
+				<line x1="3" y1="6" x2="21" y2="6"></line>
+				<line x1="3" y1="18" x2="21" y2="18"></line>
+			  </svg>
+			</a>
+		</li>
 	</ul>
-	<ul role="navigation">
-		<li>
+	<ul class="hidden-sm" role="navigation" aria-label="menu">
+		{#each menuLists as menu}
+			<li><a href="{ menu.url }">{ menu.text }</a></li>
+		{/each}
+		<!-- <li>
 			<a href="/product">Product</a>
 		</li>
 		<li>
@@ -27,7 +61,7 @@
 		</li>
 		<li>
 			<a href="/about">About Us</a>
-		</li>
+		</li> -->
 	</ul>
 </nav>
   

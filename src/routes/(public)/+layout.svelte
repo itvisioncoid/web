e466c4860486851cdb6139367cb12270b7d7e2f3
ryu-rhee <ryu-rhee@proton.me>
2023-02-27 13:30:44 +0800

    fix brand logo & navbar (#7)

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,84 +1,101 @@
 <script lang="ts">
-	// import '$src/app.scss';
-
 	const date = new Date();
 	const menuLists = [
 		{ text: 'Product', url: '/product' },
 		{ text: 'Services', url: '/services' },
 		{ text: 'Contact Us', url: '/contact' },
-		{ text: 'About Us', url: '/about' },
+		{ text: 'About Us', url: '/about' }
 	];
-	let hideNav = true;
 </script>
 
 <svelte:head>
 	<title>Home - IT Vision</title>
 </svelte:head>
 
-<div id="mobile-nav" class="mobile-nav" aria-hidden="{hideNav}" aria-label="mobile-nav" on:click|self={() => (hideNav = true)}>
-	<aside>
-		<nav>
-			<ul>
+<nav class="container">
+	<ul>
+		<li>
+			<a href="/">
+				<img src="/logo.png" alt="Company logo" aria-label="Company logo" />
+			</a>
+		</li>
+	</ul>
+	<ul class="hidden-sm-only">
+		{#each menuLists as menu}
+			<li>
+				<a href={menu.url}>{menu.text}</a>
+			</li>
+		{/each}
+	</ul>
+	<ul class="show-sm-only">
+		<li role="list" dir="rtl">
+			<button aria-haspopup="listbox">
+				<svg
+					aria-hidden="true"
+					focusable="false"
+					role="img"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					height="25px"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				>
+					<line x1="3" y1="12" x2="21" y2="12" />
+					<line x1="3" y1="6" x2="21" y2="6" />
+					<line x1="3" y1="18" x2="21" y2="18" />
+				</svg>
+			</button>
+			<ul role="listbox">
 				{#each menuLists as menu}
-				<li>
-					<a href="{ menu.url }">{ menu.text }</a>
-				</li>
+					<li>
+						<a href={menu.url}>{menu.text}</a>
+					</li>
 				{/each}
 			</ul>
-		</nav>
-	</aside>
-</div>
+		</li>
+	</ul>
+</nav>
 
-<div class="nav-container">
-
-	<nav class="container" aria-label="main-nav">
-		<ul class="brand">
-			<li>
-				<a href="/" class="contrast" aria-label="brand">
-					<strong>IT Vision</strong>
-				</a>
-			  </li>
-			<li class="show-sm-only">
-				<a href="#mobile-nav" aria-label="Close menu" aria-controls="mobile-nav" on:click|preventDefault={() => (hideNav = !hideNav)}>
-				  <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="16px" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-					<line x1="3" y1="12" x2="21" y2="12"></line>
-					<line x1="3" y1="6" x2="21" y2="6"></line>
-					<line x1="3" y1="18" x2="21" y2="18"></line>
-				  </svg>
-				</a>
-			</li>
-		</ul>
-		<ul class="hidden-sm" role="navigation" aria-label="menu">
-			{#each menuLists as menu}
-				<li><a href="{ menu.url }">{ menu.text }</a></li>
-			{/each}
-			<!-- <li>
-				<a href="/product">Product</a>
-			</li>
-			<li>
-				<a href="/services">Services</a>
-			</li>
-			<li>
-				<a href="/contact">Contact Us</a>
-			</li>
-			<li>
-				<a href="/about">About Us</a>
-			</li> -->
-		</ul>
-	</nav>
-</div>
-  
 <slot />
 
-<footer class="section">
-	<section class="container">
-		<p>&copy; <strong>CV. IT VISION</strong> { date.getFullYear() } All rights reserved</p>
-	</section>
+<footer>
+	<p>&copy; <strong>CV. IT VISION</strong> {date.getFullYear()} All rights reserved</p>
 </footer>
 
-<style>
-footer .container p {
-	text-align: center;
-}
+<style lang="scss">
+	/* TODO: how to import variabel `$breakpoints` from '$src/app.scss' */
+	$breakpoints: (
+		xs: 0,
+		sm: 576px,
+		md: 768px,
+		lg: 992px,
+		xl: 1200px
+	);
 
+	[aria-label='Company logo'] {
+		width: 150px;
+	}
+	.hidden-sm-only {
+		@media (max-width: map-get($breakpoints, 'md')) {
+			display: none;
+		}
+	}
+	.show-sm-only {
+		ul {
+			width: 50vw;
+			a {
+				padding: 1rem;
+			}
+		}
+		@media (min-width: map-get($breakpoints, 'sm')) {
+			display: none;
+		}
+	}
+
+	footer {
+		text-align: center;
+	}
 </style>

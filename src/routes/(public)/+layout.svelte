14952a5565c7d555cd6f49bd9524fdeb0f2468f4
Riel CFB <riel.cfb@gmail.com>
2023-03-07 16:50:24 +0800

    update `main layout`

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -46,62 +46,64 @@
 	<title>Home - IT Vision</title>
 </svelte:head>
 
-<nav class="container">
-	<ul>
-		<li>
-			<a href="{base ? base : '/'}" data-href="{base ? base : '/'}">
-				<img src="/logo.png" alt="Company logo" aria-label="Company logo" />
-			</a>
-		</li>
-	</ul>
-	<ul class="hidden-sm-only">
-		{#each menuLists as menu}
+<div class="nav-container">
+	<nav class="container">
+		<ul>
 			<li>
-				<a href={menu.url}>{menu.text}</a>
+				<a href="{base ? base : '/'}" data-href="{base ? base : '/'}">
+					<img src="/logo.png" alt="Company logo" aria-label="Company logo" />
+				</a>
 			</li>
-		{/each}
-		<li>
-			<button on:click={toggleDarkMode} class="outline secondary py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none focus:shadow-none">
-				<span class="fa-solid fa-circle-half-stroke" />
-			</button>
-		</li>
-	</ul>
-	<ul class="show-sm-only">
-		<li role="list" dir="rtl">
-			<button class="outline" aria-label="mobilenav-toggler" aria-haspopup="listbox">
-				<svg
-					aria-hidden="true"
-					focusable="false"
-					role="img"
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					height="25px"
-					fill="none"
-					stroke="currentColor"
-					stroke-width="2"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				>
-					<line x1="3" y1="12" x2="21" y2="12" />
-					<line x1="3" y1="6" x2="21" y2="6" />
-					<line x1="3" y1="18" x2="21" y2="18" />
-				</svg>
-			</button>
-			<ul role="listbox">
-				{#each menuLists as menu}
-					<li>
-						<a href={menu.url}>{menu.text}</a>
-					</li>
-				{/each}
+		</ul>
+		<ul class="hidden-sm-only">
+			{#each menuLists as menu}
 				<li>
-					<button on:click={toggleDarkMode} class="outline secondary py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none">
-						<span class="fa-solid fa-circle-half-stroke" />
-					</button>
+					<a href={menu.url}>{menu.text}</a>
 				</li>
-			</ul>
-		</li>
-	</ul>
-</nav>
+			{/each}
+			<li>
+				<button on:click={toggleDarkMode} class="outline {isDark ? 'contrast' : 'secondary'} py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none focus:shadow-none">
+					<span class="fa-solid fa-circle-half-stroke" />
+				</button>
+			</li>
+		</ul>
+		<ul class="show-sm-only">
+			<li role="list" dir="rtl">
+				<button class="outline" aria-label="mobilenav-toggler" aria-haspopup="listbox">
+					<svg
+						aria-hidden="true"
+						focusable="false"
+						role="img"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						height="25px"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<line x1="3" y1="12" x2="21" y2="12" />
+						<line x1="3" y1="6" x2="21" y2="6" />
+						<line x1="3" y1="18" x2="21" y2="18" />
+					</svg>
+				</button>
+				<ul role="listbox">
+					{#each menuLists as menu}
+						<li>
+							<a href={menu.url}>{menu.text}</a>
+						</li>
+					{/each}
+					<li>
+						<button on:click={toggleDarkMode} class="outline secondary py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none">
+							<span class="fa-solid fa-circle-half-stroke" />
+						</button>
+					</li>
+				</ul>
+			</li>
+		</ul>
+	</nav>
+</div>
 
 <slot />
 
@@ -123,7 +125,7 @@
 		</div>
 		<div class="grid as-footer__con-link">
 			<div>
-				<p><b>Tautan lainnya</b></p>
+				<h5>Read More</h5>
 				{#each otherLinks as otherLink}
 					<div>
 						<a href={otherLink.url}>{otherLink.text}</a>
@@ -131,7 +133,7 @@
 				{/each}
 			</div>
 			<div>
-				<p><b>Layanan yang kami tawarkan</b></p>
+				<h5>Solutions</h5>
 				{#each serviceLinks as serviceLink}
 					<div>
 						<a href={serviceLink.url}>{serviceLink.text}</a>
@@ -145,9 +147,9 @@
 <div class="as-footer">
 	<hr />
 	<div class="as-footer__boxed container">
-		<small>&copy; <span><b>CV. IT VISION</b></span> {date.getFullYear()} All rights reserved</small>
+		<small>&copy; <a href="/"><b>CV. IT VISION</b></a> {date.getFullYear()} All rights reserved</small>
 		<!-- FIXME: update base url! -->
-		<small><a href="/">Syarat dan Ketentuan</a> &bull; <a href="/">Kebijakan Privasi</a></small>
+		<small><a href="/">Terms & Conditions</a> &bull; <a href="/">Privacy Policy</a></small>
 	</div>
 </div>
 

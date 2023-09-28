ea875cdc98388923ccc80c204e82c337b1d5a832
Fiquell <fiquellh@gmail.com>
2023-09-29 02:45:38 +0800

    split footer separately

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { base } from "$app/paths";
-	const date = new Date();
+	import { base } from '$app/paths';
+	import Footer from '$src/lib/components/Footer.svelte';
+
 	let isDark: boolean;
 
 	const menuLists = [
@@ -8,26 +9,6 @@
 		{ text: 'Services', url: `${base}/services` },
 		{ text: 'Contact Us', url: `${base}/contact` },
 		{ text: 'About Us', url: `${base}/about` }
-	];
-
-	// FIXME: update base url!
-	const otherLinks = [
-		{ text: 'Blog', url: '/' },
-		{ text: 'FAQ', url: '/' },
-		{ text: 'Karier', url: '/' },
-		{ text: 'Kontak', url: '/' },
-		{ text: 'Partner', url: '/' }
-	];
-
-	const serviceLinks = [
-		{ text: 'Kelola Email', url: '/' },
-		{ text: 'Kelola LAN', url: '/' },
-		{ text: 'Kelola Pusat Data', url: '/' },
-		{ text: 'Kelola Router', url: '/' },
-		{ text: 'Pengembangan Aplikasi', url: '/' },
-		{ text: 'Pengembangan Perangkat Lunak', url: '/' },
-		{ text: 'Perangkat keras komputer', url: '/' },
-		{ text: 'Solusi TI Individu', url: '/' }
 	];
 
 	function toggleDarkMode() {
@@ -50,8 +31,8 @@
 	<nav class="container">
 		<ul>
 			<li>
-				<a href="{base ? `${base}/` : '/'}" data-href="{base ? `${base}/` : '/'}">
-					<img src="./logo.png" alt="Company logo" aria-label="Company logo" />
+				<a href={base ? `${base}/` : '/'} data-href={base ? `${base}/` : '/'}>
+					<img src="./logo.png" alt="Company logo" aria-label="Company logo" class="w-36" />
 				</a>
 			</li>
 		</ul>
@@ -62,7 +43,11 @@
 				</li>
 			{/each}
 			<li>
-				<button on:click={toggleDarkMode} class="outline {isDark ? 'contrast' : 'secondary'} py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none focus:shadow-none">
+				<button
+					on:click={toggleDarkMode}
+					class="outline {isDark
+						? 'contrast'
+						: 'secondary'} py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none focus:shadow-none">
 					<span class="fa-solid fa-circle-half-stroke" />
 				</button>
 			</li>
@@ -81,8 +66,7 @@
 						stroke="currentColor"
 						stroke-width="2"
 						stroke-linecap="round"
-						stroke-linejoin="round"
-					>
+						stroke-linejoin="round">
 						<line x1="3" y1="12" x2="21" y2="12" />
 						<line x1="3" y1="6" x2="21" y2="6" />
 						<line x1="3" y1="18" x2="21" y2="18" />
@@ -95,7 +79,9 @@
 						</li>
 					{/each}
 					<li>
-						<button on:click={toggleDarkMode} class="outline secondary py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none">
+						<button
+							on:click={toggleDarkMode}
+							class="outline secondary py-1 px-2 outline-0 outline-transparent border-none active:border-none active:outline-none active:shadow-none focus:border-none focus:outline-none">
 							<span class="fa-solid fa-circle-half-stroke" />
 						</button>
 					</li>
@@ -106,55 +92,9 @@
 </div>
 
 <slot />
-
-<footer class="as-footer">
-	<div class="grid">
-		<div>
-			<a href="/">
-				<img class="as-footer__logo" src="/logo.png" alt="Company logo" aria-label="Company logo" />
-			</a>
-			<div>
-				<p>
-					Dengan dukungan tim yang terdiri dari profesional berpengalaman dan berdedikasi.
-					<span><b>IT VISION</b></span> bertekad untuk memberikan layanan yang berkualitas tinggi dan
-					dukungan efektif untuk perusahaan dan klien kami.
-				</p>
-				<p>Garden View Residence, jalan Transmigrasi No.A8 Monjok Timur, Mataram.</p>
-				<p>Telepon: +62 82 340 709 041</p>
-			</div>
-		</div>
-		<div class="grid as-footer__con-link">
-			<div>
-				<h5>Read More</h5>
-				{#each otherLinks as otherLink}
-					<div>
-						<a href={otherLink.url}>{otherLink.text}</a>
-					</div>
-				{/each}
-			</div>
-			<div>
-				<h5>Solutions</h5>
-				{#each serviceLinks as serviceLink}
-					<div>
-						<a href={serviceLink.url}>{serviceLink.text}</a>
-					</div>
-				{/each}
-			</div>
-		</div>
-	</div>
-</footer>
-
-<div class="as-footer">
-	<hr />
-	<div class="as-footer__boxed container">
-		<small>&copy; <a href="/"><b>CV. IT VISION</b></a> {date.getFullYear()} All rights reserved</small>
-		<!-- FIXME: update base url! -->
-		<small><a href="/">Terms & Conditions</a> &bull; <a href="/">Privacy Policy</a></small>
-	</div>
-</div>
+<Footer />
 
 <style lang="scss">
-	/* TODO: how to import variabel `$breakpoints` from '$src/app.scss' */
 	$breakpoints: (
 		xs: 0,
 		sm: 576px,
@@ -162,10 +102,6 @@
 		lg: 992px,
 		xl: 1200px
 	);
-
-	[aria-label='Company logo'] {
-		width: 150px;
-	}
 	.hidden-sm-only {
 		@media (max-width: map-get($breakpoints, 'md')) {
 			display: none;
@@ -188,48 +124,10 @@
 		--form-element-spacing-horizontal: 0.6rem;
 		outline-style: none;
 
-		&:focus + ul[role=listbox],
-		&:active + ul[role=listbox],
-		&:focus-within + ul[role=listbox] {
+		&:focus + ul[role='listbox'],
+		&:active + ul[role='listbox'],
+		&:focus-within + ul[role='listbox'] {
 			display: flex;
-		}
-	}
-
-	ul[role=list]:focus-within + ul[role=listbox] ~ ul[role=listbox]{
-		display: flex;
-	}
-
-	.as-footer {
-		--grid-spacing-vertical: 1.5rem;
-		span {
-			color: var(--primary);
-		}
-		.as-footer__con-link {
-			justify-self: end;
-			width: 80%;
-			div > p {
-				margin: 1rem 0;
-			}
-			a {
-				line-height: 32px;
-			}
-			@media (max-width: map-get($breakpoints, 'lg')) {
-				width: 100%;
-			}
-		}
-		.as-footer__logo {
-			width: 250px;
-			margin-bottom: 2.5rem;
-		}
-		.as-footer__boxed {
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-			padding: 1rem 0;
-			@media (max-width: map-get($breakpoints, 'sm')) {
-				flex-direction: column;
-				gap: 0.5rem;
-			}
 		}
 	}
 </style>

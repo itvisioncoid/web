b167f0e95479a57e7f43964adfcf526e53e8711e
Fiquell <fiquellh@gmail.com>
2023-09-28 10:06:06 +0800

    update tailwindcss base configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      screens: {
-        'xs': '475px',
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px'
-      }
-    },
-  },
-  plugins: [],
-}
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		screens: {
+			xs: '475px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px'
+		},
+		extend: {}
+	},
+	plugins: []
+};
+
+module.exports = config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
-// import { defineConfig } from 'vitest/config';
 
 const config: UserConfig = {
 	logLevel: 'info',
-
 	build: {
 		minify: true
 	},
-
 	plugins: [imagetools(), sveltekit()],
-
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
